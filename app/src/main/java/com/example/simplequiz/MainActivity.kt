dd4fe599f69a46cccf13ab93f0060a63406d8230
tky3a <takayamac@icloud.com>
2021-11-21 23:08:19 +0900

    クイズアプリの動きを作成

--- app/src/main/java/com/example/simplequiz/MainActivity.kt
+++ app/src/main/java/com/example/simplequiz/MainActivity.kt
@@ -2,10 +2,121 @@ package com.example.simplequiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tvCount : TextView = findViewById(R.id.tvCount)
+        val tvQuestion : TextView = findViewById(R.id.tvQuestion)
+        val btn0 : Button = findViewById(R.id.btn0)
+        val btn1 : Button = findViewById(R.id.btn1)
+        val btn2 : Button = findViewById(R.id.btn2)
+        val btn3 : Button = findViewById(R.id.btn3)
+        var i = 0
+
+        // 配列
+        val quizData = arrayOf("A0","A1","A2","A3")
+
+        // シャッフル
+        val list = listOf(0,1,2,3)
+        val num = list.shuffled()
+
+        // ボタンにquizDataを表示
+        btn0.text = quizData[num[0]]
+        btn1.text = quizData[num[1]]
+        btn2.text = quizData[num[2]]
+        btn3.text = quizData[num[3]]
+
+        // btn0を押した時の正誤判定
+        btn0.setOnClickListener {
+            if (btn0.text == quizData[i]){
+                // 正解
+                // カウントを１増やす
+                i++
+                btn0.isEnabled = false // 無効化
+                tvCount.text = i.toString() + "問正解"
+                if (i == 4) {
+                    tvQuestion.text = "全問正解！Game Clear"
+                }
+            } else {
+                // 不正解でボタンの無効化
+                tvQuestion.text = "不正解 Game Over"
+                // ボタン無効化
+                btn0.isEnabled = false
+                btn1.isEnabled = false
+                btn2.isEnabled = false
+                btn3.isEnabled = false
+            }
+        }
+
+        // btn1を押した時の正誤判定
+        btn1.setOnClickListener {
+            if (btn1.text == quizData[i]){
+                // 正解
+                // カウントを１増やす
+                i++
+                btn1.isEnabled = false // 無効化
+                tvCount.text = i.toString() + "問正解"
+                if (i == 4) {
+                    tvQuestion.text = "全問正解！Game Clear"
+                }
+            } else {
+                // 不正解でボタンの無効化
+                tvQuestion.text = "不正解 Game Over"
+                // ボタン無効化
+                btn0.isEnabled = false
+                btn1.isEnabled = false
+                btn2.isEnabled = false
+                btn3.isEnabled = false
+            }
+        }
+
+        // btn0を押した時の正誤判定
+        btn2.setOnClickListener {
+            if (btn2.text == quizData[i]){
+                // 正解
+                // カウントを１増やす
+                i++
+                btn2.isEnabled = false // 無効化
+                tvCount.text = i.toString() + "問正解"
+                if (i == 4) {
+                    tvQuestion.text = "全問正解！Game Clear"
+                }
+            } else {
+                // 不正解でボタンの無効化
+                tvQuestion.text = "不正解 Game Over"
+                // ボタン無効化
+                btn0.isEnabled = false
+                btn1.isEnabled = false
+                btn2.isEnabled = false
+                btn3.isEnabled = false
+            }
+        }
+
+        // btn0を押した時の正誤判定
+        btn3.setOnClickListener {
+            if (btn3.text == quizData[i]){
+                // 正解
+                // カウントを１増やす
+                i++
+                btn3.isEnabled = false // 無効化
+                tvCount.text = i.toString() + "問正解"
+                if (i == 4) {
+                    tvQuestion.text = "全問正解！Game Clear"
+                }
+            } else {
+                // 不正解でボタンの無効化
+                tvQuestion.text = "不正解 Game Over"
+                // ボタン無効化
+                btn0.isEnabled = false
+                btn1.isEnabled = false
+                btn2.isEnabled = false
+                btn3.isEnabled = false
+            }
+        }
     }
 }
